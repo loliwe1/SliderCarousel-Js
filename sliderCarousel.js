ea61704d173e46287c8d4188eb1064042f883917
Stanislav Vlasov <vvs00794@gmail.com>
2020-01-04 23:16:19 +0400

    update class SliderCarousel

--- sliderCarousel.js
+++ sliderCarousel.js
@@ -1,61 +1,58 @@
 'use strict';
 class SliderCarousel {
 
-    constructor(slider, sliderItem, slidesToShow, nextSlide, prevSlide, position = 0) {
+    constructor(sliderWrapper, slider, sliderItem, slidesToShow, slidesToScroll = 1, nextSlide, prevSlide, position = 0) {
+        this.slidesToScroll = slidesToScroll;
+        this.sliderWrapper = document.querySelector(sliderWrapper);
         this.slider = document.querySelector(slider);
         this.sliderItem = document.querySelectorAll(sliderItem);
         this.slidesToShow = slidesToShow;
         this.nextSlide = document.querySelector(nextSlide);
         this.prevSlide = document.querySelector(prevSlide);
         this.position = position;
+
     }
 
     sliderStyle() {
         const style = document.createElement('style');
         style.innerHTML = `
+            .slider__wrapper-style {
+                overflow: hidden;
+                width: ${this.sliderItem[0].clientWidth * this.slidesToShow}px;
+            }
            .slider-style {
                display: flex;
-               overflow: hidden;
-               width: ${this.sliderItem[0].clientWidth * this.slidesToShow}px;
-               flex-grow: 1;
            } 
            .slider__item-style {
                flex: 0 0 ${100 / this.slidesToShow}%;
                margin: 0;
            }`;
-        
+
         document.head.append(style);
+
+        this.sliderWrapper.classList.add('slider__wrapper-style');
         this.slider.classList.add('slider-style');
 
         this.sliderItem.forEach(slide => {
             slide.classList.add('slider__item-style')
         });
-
-       
-
     }
 
     nextSlideF() {
         this.position += 1;
         this.slider.style.transition = `1s all`;
-        this.slider.style.transform = `translateX(${this.sliderItem[0].clientWidth * position})`;
-        console.log(position)
+        this.slider.style.transform = `translateX(-${this.sliderItem[0].clientWidth * this.position * this.slidesToScroll}px)`;
     }
 
     prevSlideF() {
         this.position -= 1;
-        this.slider.style.transition = `1s translate`;
-        this.slider.style.transform = `translateX(${this.sliderItem[0].clientWidth * position})`;
-        console.log(position)
+        this.slider.style.transition = `1s all`;
+        this.slider.style.transform = `translateX(-${this.sliderItem[0].clientWidth * this.position * this.slidesToScroll}px)`;
     };
 
     start() {
         this.sliderStyle();
-        this.nextSlide.addEventListener('click', this.nextSlideF);
-        this.prevSlide.addEventListener('click', this.prevSlideF);
-
-        
+        this.nextSlide.addEventListener('click', this.nextSlideF.bind(this));
+        this.prevSlide.addEventListener('click', this.prevSlideF.bind(this));
     }
-
-
 }
